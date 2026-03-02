42092a0a10dbd6bba99c6c6e8af8a76ac7e27a1a
Aungkokolin1997 <lin@quartile.co>
2026-03-02 04:17:41 +0000

    [IMP] report_qweb_field_option: add domain field for better controlling the decimal places
    Add domain field in report config for better control of decimal places.

--- report_qweb_field_option/models/qweb_field_options.py
+++ report_qweb_field_option/models/qweb_field_options.py
@@ -4,8 +4,10 @@
 import ast
 import logging
 
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 from odoo.exceptions import ValidationError
+from odoo.fields import Domain
+from odoo.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
 
@@ -52,6 +54,14 @@ class QwebFieldOptions(models.Model):
     )
     digits = fields.Integer()
     company_id = fields.Many2one("res.company", string="Company")
+    domain = fields.Char(
+        help="Optional domain for additional filtering conditions.\n"
+        "This is evaluated in addition to UoM/Currency conditions.\n"
+        "Examples:\n"
+        "[('secondary_uom_id', '=', 1)]\n"
+        "[('secondary_uom_id.name', '=', 'Box')]\n"
+        "[('state', 'in', ['sale', 'done'])]",
+    )
 
     @api.constrains("field_options")
     def _check_field_options_format(self):
@@ -77,6 +87,31 @@ class QwebFieldOptions(models.Model):
                     )
                 )
 
+    def _get_eval_context(self):
+        return {
+            "time": tools.safe_eval.time,
+            "datetime": tools.safe_eval.datetime,
+            "dateutil": tools.safe_eval.dateutil,
+            "timezone": tools.safe_eval.pytz.timezone,
+            "context_today": lambda: fields.Date.context_today(self),
+        }
+
+    @api.constrains("domain")
+    def _check_domain_format(self):
+        for rec in self:
+            if not rec.domain:
+                continue
+            try:
+                Domain(safe_eval(rec.domain, rec._get_eval_context()))
+            except Exception as e:
+                raise ValidationError(
+                    self.env._(
+                        "Invalid domain format: %(domain)s.\nError: %(error)s",
+                        domain=rec.domain,
+                        error=e,
+                    )
+                ) from e
+
     def _get_score(self, record):
         self.ensure_one()
         score = 1
@@ -93,6 +128,20 @@ class QwebFieldOptions(models.Model):
             if record[self.currency_field_id.sudo().name] == self.currency_id:
                 score += 1
             else:
+                return -1
+        if self.domain:
+            try:
+                domain = Domain(safe_eval(self.domain, self._get_eval_context()))
+                if not record.filtered_domain(domain):
+                    return -1
+                score += 1
+            except Exception as e:
+                _logger.warning(
+                    "Failed to evaluate domain %s for record %s: %s",
+                    self.domain,
+                    record,
+                    e,
+                )
                 return -1
         return score
 
